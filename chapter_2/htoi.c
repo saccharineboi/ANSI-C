2b21ae75533e1d83880063d9c600b6511701e8c9
saccharineboi <77533355+saccharineboi@users.noreply.github.com>
2021-02-13 20:39:52 +0400

    added htoi.c

---
+++ chapter_2/htoi.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int htoi(const char s[])
+{
+        int i = 0, n = 0;
+
+        if (s[i] == '0')
+                ++i;
+        if (s[i] == 'x' || s[i] == 'X')
+                ++i;
+        else if (i == 1)
+                return n;
+
+        for (; s[i]; ++i) {
+              if (isdigit(s[i]))
+                      n = 16 * n + (s[i] - '0');
+              else if (isalpha(s[i]))
+                      n = 16 * n + (s[i] - (isupper(s[i]) ? 'A' : 'a')) + 10;
+              else
+                      return n;
+        }
+        return n;
+}
+
+// ex 2-3
+int main()
+{
+        printf("%d\n", htoi("0xa0"));
+
+        return 0;
+}
