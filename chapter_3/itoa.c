54fbdf85be658bbd47cee087e41d7abd5bd37806
saccharineboi <77533355+saccharineboi@users.noreply.github.com>
2021-02-21 18:54:15 +0400

    added itoa.c

---
+++ chapter_3/itoa.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <limits.h>
+
+void reverse(char s[])
+{
+        int tmp;
+        for (int i = 0, j = strlen(s) - 1; i < j; ++i, --j)
+                tmp = s[i], s[i] = s[j], s[j] = tmp;
+}
+
+void itoa(long n, char s[])
+{
+        int i, sign;
+
+        if ((sign = n) < 0)
+                n = -n;
+
+        i = 0;
+        do
+                s[i++] = n % 10 + '0';
+        while ((n /= 10) > 0);
+
+        if (sign < 0)
+                s[i++] = '-';
+        s[i] = 0;
+        reverse(s);
+}
+
+int main()
+{
+        int x = INT_MIN;
+
+        char buffer[100];
+        itoa(x, buffer);
+        printf("%s\n", buffer);
+        printf("%d\n", x);
+
+        return 0;
+}
